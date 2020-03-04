0be69be485cf870ce400f9a457bed5a899eb0c0a
Vrndavana <Vrmdavama.th@gmail.com>
2020-03-03 16:36:31 -0800

    Welcome: Welcome to Plant Quencher!

--- water-plants/src/Components/Welcome.js
+++ water-plants/src/Components/Welcome.js
@@ -5,7 +5,8 @@ export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
-        <h1>Welcome to water my plants!</h1>
+        <h1>Welcome to Plant Quencher! Log in or Sign Up!</h1>
+        <h4>Show your plants you care</h4>
       </header>
     </section>
   );
